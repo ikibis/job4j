e2f76b79bcef2b208ff200dab79b1501ea5d5d38
ikibis <i.kibis@gmail.com>
2018-08-09 22:27:05 +0500

    1.3. JavDoc. Code convetion

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j.calculate;
 
-public class Calculate{ 
+/**
+ * Class Calculate решение задачи части 001 урок1.
+ * @author kibis
+ * @since 09.08.2018
+ * @version 1
+ */
+public class Сalculate {
 
+       /**
+	* Main
+	* @param args - args.
+	* @return вывод Фразы "Hello world"
+	*/
 	public static void main(String[] args)
 	{
 
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Ilya Kibis (mailto:ilya.kibis@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
