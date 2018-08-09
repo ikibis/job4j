475afd678c4cf8607bf2dc9effa3b7ba0c7ae7cf
ikibis <i.kibis@gmail.com>
2018-08-10 00:00:03 +0500

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -6,8 +6,7 @@ package ru.job4j.calculate;
  * @since 09.08.2018
  * @version 1
  */
-public class Сalculate {
-
+public class Calculate {
        /**
 	* Main
 	* @param args - args.
@@ -15,9 +14,14 @@ public class Сalculate {
 	*/
 	public static void main(String[] args)
 	{
-
-		System.out.println("Hello world");
-	
+		System.out.println("Hello world");	
 	}
-	
+	/**
+	* Method echo.
+	* @param name Your name.
+	* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+    		return "Echo, echo, echo : " + name;
+	}	
 }
