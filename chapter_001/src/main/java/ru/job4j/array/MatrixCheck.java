6122d10334223c1103e456e4a0649fcf3b56ac2c
ikibis <i.kibis@gmail.com>
2018-08-18 10:13:12 +0500

    6.7. Квадратный массив заполнен true или false по диагоналям

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -5,23 +5,23 @@ package ru.job4j.array;
 * @since 0.1
 */
 public class MatrixCheck {
-    /**
-      * Метод должен проверить, что все элементы по диагоналям равны true или false.
-      * @param  data входной массив.
-      * @return – результат - результат проверки.
-      */
-    public boolean mono(final boolean[][] data) {
-        boolean result = true;
-        for (int i = 0; i < data.length; i++) {
-            if (data[0][0] != data[i][i]) {
-                result = false;
-                break;
-            }
-            if (data[0][0] != data[i][data.length - i - 1]) {
-                result = false;
-                break;
-            }
-        }
-       return result;
-    }
+/**
+* Метод должен проверить, что все элементы по диагоналям равны true или false.
+* @param  data входной массив.
+* @return результат - результат проверки.
+*/
+	public boolean mono(final boolean[][] data) {
+        	boolean result = true;
+        	for (int i = 0; i < data.length; i++) {
+            		if (data[0][0] != data[i][i]) {
+                		result = false;
+                		break;
+            		}
+            		if (data[0][data.length - 1] != data[i][data.length - i - 1]) {
+                		result = false;
+                		break;
+            		}
+        	}
+        	return result;
+	}
 }
--- chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -1,10 +1,8 @@
 package ru.job4j.array;
-
-import org.junit.Test;
+ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
-public class MatrixCheckTest {
+ public class MatrixCheckTest {
     @Test
     public void whenDataMonoByTrueThenTrue() {
         MatrixCheck check = new MatrixCheck();
@@ -16,7 +14,6 @@ public class MatrixCheckTest {
         boolean result = check.mono(input);
         assertThat(result, is(true));
     }
-
     @Test
     public void whenDataNotMonoByTrueThenFalse() {
         MatrixCheck check = new MatrixCheck();
@@ -24,6 +21,31 @@ public class MatrixCheckTest {
                 {true, true, false},
                 {false, false, true},
                 {true, false, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenDataMonoByTrueThenTrue1() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, false, false, false},
+                {false, true, false, false},
+                {false, false, true, false},
+                {false, true, false, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+    @Test
+    public void whenDataNotMonoByTrueThenFalse2() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, false, false, false},
+                {false, true, true, false},
+                {false, false, true, false},
+                {false, true, false, true}
         };
         boolean result = check.mono(input);
         assertThat(result, is(false));
