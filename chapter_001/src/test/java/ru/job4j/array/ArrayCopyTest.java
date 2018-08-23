aad7c465afc0eedd582b3899e3ebdf57267d8817
ikibis <i.kibis@gmail.com>
2018-08-23 22:21:48 +0500

    Дополнительное задание

--- chapter_001/src/test/java/ru/job4j/array/ArrayCopyTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCopyTest.java
@@ -10,7 +10,7 @@ public class ArrayCopyTest {
         ArrayCopy arrc = new ArrayCopy();
         int[] source1 = {1, 3, 5, 7, 9};
         int[] source2 = {2, 4, 6, 8, 10};
-        int[] result = arrc.consolidation(source1, source2);
+        int[] result = arrc.join(source1, source2);
         int[] correctly = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         assertThat(result, is(correctly));
     }
@@ -19,7 +19,7 @@ public class ArrayCopyTest {
         ArrayCopy arrc = new ArrayCopy();
         int[] source1 = {1, 1, 5, 7, 9};
         int[] source2 = {1, 2, 2, 8, 9};
-        int[] result = arrc.consolidation(source1, source2);
+        int[] result = arrc.join(source1, source2);
         int[] correctly = {1, 1, 1, 2, 2, 5, 7, 8, 9, 9};
         assertThat(result, is(correctly));
     }
@@ -28,7 +28,7 @@ public class ArrayCopyTest {
         ArrayCopy arrc = new ArrayCopy();
         int[] source1 = {1, 1, 5, 7, 9};
         int[] source2 = {1, 2, 2};
-        int[] result = arrc.consolidation(source1, source2);
+        int[] result = arrc.join(source1, source2);
         int[] correctly = {1, 1, 1, 2, 2, 5, 7, 9};
         assertThat(result, is(correctly));
     }
