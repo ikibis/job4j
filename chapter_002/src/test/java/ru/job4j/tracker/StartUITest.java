125791a074e18c4d53bafb8e53465162f0eaf1fc
ikibis <i.kibis@gmail.com>
2018-08-27 20:13:19 +0500

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,10 +1,29 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
         Tracker tracker = new Tracker();
