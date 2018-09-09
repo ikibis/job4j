a3dc9aab768d2efcb33cd115b2c3dad3d92ff54d
ikibis <i.kibis@gmail.com>
2018-09-09 15:56:16 +0500

    Каркас шахматной доски

--- chapter_002/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chapter_002/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -23,12 +23,15 @@ public class BishopBlack implements Figure {
 
     @Override
     public Cell[] way(Cell source, Cell dest)  {
+        if (Math.abs(source.x - dest.x) != Math.abs(source.y - dest.y)) {
+            throw new IllegalStateException("an Bishop can't walk like that");
+        }
         int size = Math.abs(source.x - dest.x);
         Cell[] steps = new Cell[size];
         int deltaX = Integer.compare(dest.x, source.x);
         int deltaY = Integer.compare(dest.y, source.y);
         for (int index = 0; index != size; index++) {
-            steps[index] = Cell.find(source.x + deltaX * (++index), source.y + deltaY * (index--));
+            steps[index] = Cell.find(source.x + deltaX * (index + 1), source.y + deltaY * (index + 1));
         }
         return steps;
     }
