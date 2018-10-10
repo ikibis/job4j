1dc7ead4cf32c989fafae34def17d2ed8a0c6763
ikibis <i.kibis@gmail.com>
2018-10-10 11:41:58 +0500

    Stream API дополнительное задание

--- chapter_004/src/main/java/ru/job4j/function/Stream.java
+++ chapter_004/src/main/java/ru/job4j/function/Stream.java
@@ -4,6 +4,6 @@ import java.util.Arrays;
 
 public class Stream {
     public int demo(int[] i) {
-        return Arrays.stream(i).filter(x -> x % 2 ==0).map(x -> x*x).reduce(0, Integer::sum);
+        return Arrays.stream(i).filter(x -> x % 2 == 0).map(x -> x * x).reduce(0, Integer::sum);
     }
 }
