cd4c050e58cc3a820f7b9ad028de45b59fdcff23
ikibis <i.kibis@gmail.com>
2018-10-10 10:59:40 +0500

    5.2.2. Реализовать Store<T extends Base>

--- chapter_005/src/main/java/ru/job4j/generic/RoleStore.java
+++ chapter_005/src/main/java/ru/job4j/generic/RoleStore.java
@@ -1,39 +1,7 @@
 package ru.job4j.generic;
 
 public class RoleStore<T> extends AbstractStore {
-    private SimpleArray<Role> storeArray;
-
     public RoleStore(int size) {
-        this.storeArray = new SimpleArray<>(size);
-    }
-
-    @Override
-    public void add(Base model) {
-        storeArray.add((Role) model);
-    }
-
-    @Override
-    public boolean replace(String id, Base model) {
-        boolean result = false;
-        if (storeArray.get(Integer.parseInt(id)) != null) {
-            storeArray.set(Integer.parseInt(id), (Role) model);
-            result = true;
-        }
-        return result;
-    }
-
-    @Override
-    public boolean delete(String id) {
-        boolean result = false;
-        if (storeArray.get(Integer.parseInt(id)) != null) {
-            storeArray.delete(Integer.parseInt(id));
-            result = true;
-        }
-        return result;
-    }
-
-    @Override
-    public Base findById(String id) {
-        return storeArray.get(Integer.parseInt(id));
+        super(size);
     }
 }
