be86110e361a90644970b1261e3a0081d029c8f7
ikibis <i.kibis@gmail.com>
2018-10-15 15:25:02 +0500

    5. Перекрывать и equals и hashCode

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -14,6 +14,30 @@ public class User {
         this.birthday = birthday;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -27,10 +51,10 @@ public class User {
                 && Objects.equals(name, user.name)
                 && Objects.equals(birthday, user.birthday);
     }
-/*
+
     @Override
     public int hashCode() {
         return Objects.hash(name, children, birthday);
     }
-*/
+
 }
