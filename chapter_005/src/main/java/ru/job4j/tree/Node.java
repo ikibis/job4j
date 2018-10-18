2aa978820d6a9c918454c6b512c1284aede299fe
ikibis <i.kibis@gmail.com>
2018-10-18 10:10:08 +0500

    1. Создать элементарную структуру дерева

--- chapter_005/src/main/java/ru/job4j/tree/Node.java
+++ chapter_005/src/main/java/ru/job4j/tree/Node.java
@@ -1,13 +1,12 @@
 package ru.job4j.tree;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
-public class Node<E extends Comparable<E>> {
+public class Node<E extends Comparable<E>> implements SimpleTree<E>{
     private final List<Node<E>> children = new ArrayList<>();
     private final E value;
-
+    private Node<E> root;
     public Node(final E value) {
         this.value = value;
     }
@@ -23,5 +22,33 @@ public class Node<E extends Comparable<E>> {
     public boolean eqValue(E that) {
         return this.value.compareTo(that) == 0;
 
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        return false;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.eqValue(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            for (Node<E> child : el.leaves()) {
+                data.offer(child);
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
     }
 }
--- chapter_005/src/test/java/ru/job4j/tree/TreeTest.java
+++ chapter_005/src/test/java/ru/job4j/tree/TreeTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class TreeTest<E> {
     @Test
     public void when6ElFindLastThen6() {
-        SimpleTree<Integer> tree = new Node<E>(1);
+        SimpleTree<Integer> tree = new Node<>(1);
         tree.add(1, 2);
         tree.add(1, 3);
         tree.add(1, 4);
@@ -21,7 +21,7 @@ public class TreeTest<E> {
 
     @Test
     public void when6ElFindNotExitThenOptionEmpty() {
-        SimpleTree<Integer> tree = new SimpleTreeClass<>(1);
+        SimpleTree<Integer> tree = new Node<>(1);
         tree.add(1, 2);
         assertThat(
                 tree.findBy(7).isPresent(),
