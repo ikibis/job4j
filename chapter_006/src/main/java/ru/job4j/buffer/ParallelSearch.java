dd37abff427933d7f114b2c471129c4b85bd8204
ikibis <i.kibis@gmail.com>
2018-10-30 15:00:21 +0500

    2. Обеспечить остановку потребителя.

---
+++ chapter_006/src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,35 @@
+package ru.job4j.buffer;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+@ThreadSafe
+public class ParallelSearch {
+    @GuardedBy("this")
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (true) {
+                        System.out.println(queue.poll());
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+        ).start();
+    }
+}
