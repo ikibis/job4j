b88c2f95ca51cff499a07ee25da823947e99ac57
ikibis <ilya.kibis@gmail.com>
2018-11-08 22:39:19 +0500

    Игра бомбермен

--- chapter_006/src/main/java/ru/job4j/bomberman/Hero.java
+++ chapter_006/src/main/java/ru/job4j/bomberman/Hero.java
@@ -1,20 +1,19 @@
 package ru.job4j.bomberman;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Hero implements Runnable {
     ReentrantLock position;
-    final Random random = new Random();
+    Queue<ReentrantLock> steps = new LinkedList<>();
 
     public void setPosition(ReentrantLock position) {
         this.position = position;
+        position.lock();
     }
 
-    public synchronized List<ReentrantLock> way() {
+    public synchronized void way() {
         int x = 0;
         int y = 0;
         for (int i = 0; i < 10; i++) {
@@ -22,10 +21,10 @@ public class Hero implements Runnable {
                 if ((Board.board[i][j]).equals(this.position)) {
                     x = i;
                     y = j;
+                    break;
                 }
             }
         }
-        List<ReentrantLock> steps = new ArrayList<>();
         if (x - 1 >= 0) {
             steps.add(Board.board[x - 1][y]);
         }
@@ -38,37 +37,42 @@ public class Hero implements Runnable {
         if (y + 1 < Board.board.length) {
             steps.add(Board.board[x][y + 1]);
         }
-        return steps;
+        System.out.println(steps);
     }
+
     public boolean move(ReentrantLock source, ReentrantLock dest) {
         boolean result = false;
         if (source.equals(this.position)) {
             try {
-                source.tryLock(500, TimeUnit.MILLISECONDS);
+                ReentrantLock moveDest = dest;
+                while (!steps.isEmpty()) {
+                    if (moveDest.tryLock(500, TimeUnit.MILLISECONDS)) {
+                        this.position = moveDest;
+                        source.unlock();
+                        result = true;
+                    } else {
+                        moveDest = steps.poll();
+                    }
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            source.unlock();
-            source = dest;
-            source.lock();
-            result = true;
         }
+        steps = new LinkedList<>();
         return result;
     }
+
     @Override
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
-            List<ReentrantLock> steps = way();
-            ReentrantLock dest = null;
-            while (dest == null) {
-                    dest = steps.get(random.nextInt(steps.size()));
-            }
             try {
-                this.move(this.position, dest);
                 Thread.sleep(1000);
+                this.way();
+                this.move(this.position, steps.poll());
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
 }
+
