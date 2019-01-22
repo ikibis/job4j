a728411c9d3c5fd11be8b18ae4599e3d012ded97
ikibis <ilya.kibis@gmail.com>
2019-01-22 06:30:10 +0500

    4.3. XML XSLT JDBC Оптимизация

--- chapter_007/src/test/java/ru/job4j/array/SQLConnectorTest.java
+++ chapter_007/src/test/java/ru/job4j/array/SQLConnectorTest.java
@@ -12,7 +12,7 @@ public class SQLConnectorTest {
     public void createConnection() {
         program = new Program();
     }
-/*
+
     @Test
     public void startProgram() throws URISyntaxException {
         long startTime = System.currentTimeMillis();
@@ -20,7 +20,7 @@ public class SQLConnectorTest {
         long timeSpent = System.currentTimeMillis() - startTime;
         System.out.println("программа выполнялась " + timeSpent / 1000 + " секунд");
     }
-*/
+
     @Test
     public void propertiesTest() {
         Config conf = new Config();
