68b1c48494a7fd14e71591a4186f30812ba8a822
ikibis <ilya.kibis@gmail.com>
2019-02-09 12:36:55 +0500

    1. Создать web проект

--- chapter_008/src/main/java/ru/job4j/echo/EchoServlet.java
+++ chapter_008/src/main/java/ru/job4j/echo/EchoServlet.java
@@ -1,6 +1,5 @@
 package ru.job4j.echo;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,7 +8,7 @@ import java.io.PrintWriter;
 
 public class EchoServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
         writer.append("hello world");
