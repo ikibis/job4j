7dffeeed1fc2c83f0f263da537e26aacfb68aa5d
ikibis <ilya.kibis@gmail.com>
2019-02-23 18:01:09 +0500

    1. Реализовать MVC JSTL

--- chapter_008/src/main/java/ru/job4j/servlets/storage/ValidateService.java
+++ chapter_008/src/main/java/ru/job4j/servlets/storage/ValidateService.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class ValidateService {
     private static ValidateService service;
-    private final Store memory = DbStore.getInstance();
+    private final Store memory = MemoryStore.getInstance();
 
 
     public static ValidateService getInstance() {
--- chapter_008/src/main/webapp/WEB-INF/view/users.jsp
+++ chapter_008/src/main/webapp/WEB-INF/view/users.jsp
@@ -1,7 +1,7 @@
 <%@ page import="ru.job4j.servlets.model.User" %>
 <%@ page import="java.util.List" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
 <head>
     <title>Users Store</title>
