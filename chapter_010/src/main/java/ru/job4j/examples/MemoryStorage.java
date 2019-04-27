d362f5b1893f3d3b5f7ed0d7ecdf9f772864ba48
ikibis <ilya.kibis@gmail.com>
2019-04-27 13:06:50 +0500

    2. Реализовать различные виды хранилищ.

--- chapter_010/src/main/java/ru/job4j/examples/MemoryStorage.java
+++ chapter_010/src/main/java/ru/job4j/examples/MemoryStorage.java
@@ -1,12 +1,37 @@
 package ru.job4j.examples;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class MemoryStorage implements Storage {
+    private final List<User> users;
+
+    @Autowired
+    public MemoryStorage() {
+        users = new CopyOnWriteArrayList<>();
+    }
 
     @Override
-    public void add(User user) {
-        System.out.println("User stored to memory ");
+    public User add(User user) {
+        if (user != null) {
+            users.add(user);
+        }
+        return user;
+    }
+    @Override
+    public List<User> findAll() {
+        return users;
+    }
+
+    @Override
+    public User findById(int id) {
+        return users.stream()
+                .filter(user -> user.getId() == id)
+                .findAny()
+                .orElse(null);
     }
 }
--- chapter_010/src/test/java/ru/job4j/examples/UserStorageTest.java
+++ chapter_010/src/test/java/ru/job4j/examples/UserStorageTest.java
@@ -4,15 +4,11 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 public class UserStorageTest {
-    @Test
-    public void whenAddUserToStorageShouldSafeIt() {
-        MemoryStorage memory = new MemoryStorage();
-        UserStorage storage = new UserStorage(memory);
-        storage.add(new User());
-    }
 
     @Test
     public void whenLoadContextShouldGetMemoryStorage() {
@@ -25,8 +21,24 @@ public class UserStorageTest {
     @Test
     public void whenLoadContextShouldGetUserStorage() {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
-        UserStorage memory = context.getBean(UserStorage.class);
+        MemoryStorage memory = context.getBean(MemoryStorage.class);
         memory.add(new User());
         assertNotNull(memory);
+    }
+
+    @Test
+    public void whenAddUserToMemoryShouldSafeIt() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+        MemoryStorage memory = context.getBean(MemoryStorage.class);
+        User user = memory.add(new User("Mark"));
+        assertThat(memory.findById(user.getId()), is(user));
+    }
+
+    @Test
+    public void whenAddUserToDbShouldSafeIt() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+        DbStorage memory = context.getBean(DbStorage.class);
+        User user = memory.add(new User("Eva"));
+        assertThat(memory.findById(user.getId()).getName(), is(user.getName()));
     }
 }
