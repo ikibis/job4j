f8d5995bccedeef3fd1ddff60a5637ee933f0e15
ikibis <ilya.kibis@gmail.com>
2019-06-01 19:25:59 +0500

    1. Динамическое перераспределение продуктов

--- chapter_011/src/main/java/ru/job4j/lsp/ControlQuality.java
+++ chapter_011/src/main/java/ru/job4j/lsp/ControlQuality.java
@@ -3,6 +3,9 @@ package ru.job4j.lsp;
 import ru.job4j.lsp.product.Food;
 import ru.job4j.lsp.storage.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ControlQuality {
     private final StorageFactory factory = StorageFactory.getInstance();
 
@@ -11,6 +14,17 @@ public class ControlQuality {
             if (store.accept(food)) {
                 store.add(food);
             }
+        }
+    }
+
+    public void resort() {
+        List<Food> foods = new ArrayList<>();
+        List<Storage> storageList = factory.getStorageList();
+        for (Storage store : storageList) {
+            foods.addAll(store.getStorage());
+        }
+        for (Food food : foods) {
+            this.add(food);
         }
     }
 }
