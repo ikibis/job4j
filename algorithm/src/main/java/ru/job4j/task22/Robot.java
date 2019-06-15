7e0a224581d932f28d4bd2fa948c008a352c1964
ikibis <ilya.kibis@gmail.com>
2019-06-15 14:24:37 +0500

    Задан двухмерных массив. Массив заполнен числами. По массиву двигается робот.

--- algorithm/src/main/java/ru/job4j/task22/Robot.java
+++ algorithm/src/main/java/ru/job4j/task22/Robot.java
@@ -3,98 +3,49 @@ package ru.job4j.task22;
 import java.util.*;
 
 public class Robot {
+    private Map<String, Integer> wayMap = new HashMap<>();
+    private Map<String, List<Vertex>> wayPoints = new HashMap<>();
+    private Stack<Vertex> way = new Stack<>();
+
     public int optimalWay(int[][] board, int sx, int sy, int fx, int fy) {
+        int result = -1;
         Vertex start = new Vertex(board[sy][sx], sx, sy);
         Vertex finish = new Vertex(board[fy][fx], fx, fy);
-        Map<String, List<Vertex>> wayMap = fillVertexMap(board);
-        List<LinkedList<Vertex>> allWays = this.fillWayList(wayMap, start, finish);
-        List<LinkedList<Vertex>> filteredWays = this.filterWays(allWays, finish);
-        return this.shortestWay(filteredWays);
-    }
-
-    public int shortestWay(List<LinkedList<Vertex>> filteredWays) {
-        Map<Integer, LinkedList<Vertex>> map = new HashMap<>();
-        for (LinkedList<Vertex> list : filteredWays) {
-            int way = 0;
-            Vertex[] arr = list.toArray(new Vertex[0]);
-            for (int i = 0; i < arr.length - 1; i++) {
-                if (arr[i].getValue() < arr[i + 1].getValue()) {
-                    way += arr[i + 1].getValue() - arr[i].getValue();
-                }
+        wayMap.put(start.toString(), 0);
+        way.push(start);
+        wayPoints.put(start.toString(), List.of(start));
+        while (!way.empty()) {
+            Vertex currentVertex = way.pop();
+            int x = currentVertex.x;
+            int y = currentVertex.y;
+            if (y - 1 >= 0) {
+                Vertex upVertex = new Vertex(board[y - 1][x], x, y - 1);
+                this.checkVertex(currentVertex, upVertex);
             }
-            map.put(way, list);
-        }
-        int key = new ArrayList<>(map.keySet()).get(0);
-        System.out.println(key + ". Way : " + map.get(key));
-        return key;
-    }
-
-
-    public List<LinkedList<Vertex>> filterWays(List<LinkedList<Vertex>> allWays, Vertex finish) {
-        List<LinkedList<Vertex>> filteredResult = new LinkedList<>();
-        for (LinkedList<Vertex> list : allWays) {
-            if (finish.equals(list.getLast())) {
-                filteredResult.add(list);
+            if (y + 1 < board.length) {
+                Vertex downVertex = new Vertex(board[y + 1][x], x, y + 1);
+                this.checkVertex(currentVertex, downVertex);
+            }
+            if (x + 1 < board[0].length) {
+                Vertex rightVertex = new Vertex(board[y][x + 1], x + 1, y);
+                this.checkVertex(currentVertex, rightVertex);
             }
         }
-        return filteredResult;
-    }
-
-    public List<LinkedList<Vertex>> fillWayList(Map<String, List<Vertex>> wayMap, Vertex start, Vertex finish) {
-        List<LinkedList<Vertex>> result = new LinkedList<>();
-        Stack<Vertex> stack = new Stack<>();
-        stack.push(start);
-        LinkedList<Vertex> linkedList = new LinkedList<>();
-        linkedList.add(start);
-        result.add(linkedList);
-        while (!stack.empty()) {
-            Vertex currentVertex = stack.pop();
-            if (currentVertex.equals(finish)) {
-                continue;
-            }
-            List<LinkedList<Vertex>> resultCopy = new LinkedList<>(result);
-            List<Vertex> list = wayMap.get(currentVertex.getX() + ":" + currentVertex.getY());
-            for (Vertex vert : list) {
-                for (LinkedList<Vertex> list1 : result) {
-                    if (list1.getLast().equals(currentVertex) && !list1.contains(vert)) {
-                        LinkedList<Vertex> newList = new LinkedList<>(list1);
-                        newList.add(vert);
-                        if (!resultCopy.contains(newList)) {
-                            resultCopy.add(newList);
-                            stack.push(vert);
-                        }
-                    }
-                }
-            }
-            result = resultCopy;
-        }
+        result = wayMap.get(finish.toString());
+        System.out.println(result + " Optimal Way : " + wayPoints.get(finish.toString()));
         return result;
     }
 
-    public Map<String, List<Vertex>> fillVertexMap(int[][] board) {
-        int vert = board.length;
-        int horizon = board[0].length;
-        Map<String, List<Vertex>> wayMap = new HashMap<>();
-        for (int y = 0; y < vert; y++) {
-            for (int x = 0; x < horizon; x++) {
-                List<Vertex> way = new ArrayList<>();
-                Vertex currentVertex = new Vertex(board[y][x], x, y);
-                if (y - 1 >= 0) {
-                    Vertex upVertex = new Vertex(board[y - 1][x], x, y - 1);
-                    way.add(upVertex);
-                }
-                if (y + 1 < vert) {
-                    Vertex downVertex = new Vertex(board[y + 1][x], x, y + 1);
-                    way.add(downVertex);
-                }
-                if (x + 1 < horizon) {
-                    Vertex rightVertex = new Vertex(board[y][x + 1], x + 1, y);
-                    way.add(rightVertex);
-                }
-                String name = currentVertex.getX() + ":" + currentVertex.getY();
-                wayMap.put(name, way);
-            }
+    void checkVertex(Vertex current, Vertex next) {
+        int value = next.value - current.value;
+        value = value < 0 ? 0 : value;
+        int sumValue = wayMap.get(current.toString());
+        if (wayMap.get(next.toString()) == null || wayMap.get(next.toString()) > sumValue + value) {
+            way.push(next);
+            wayMap.put(next.toString(), sumValue + value);
+            List<Vertex> points = new ArrayList<>(wayPoints.get(current.toString()));
+            points.add(next);
+            wayPoints.put(next.toString(), points);
         }
-        return wayMap;
     }
 }
--- algorithm/src/test/java/ru/job4j/task22/RobotTest.java
+++ algorithm/src/test/java/ru/job4j/task22/RobotTest.java
@@ -13,15 +13,21 @@ public class RobotTest {
     @Before
     public void prepare() {
         board = new int[][]{
-                {1, 2, 3},
+                {1, 6, 3},
                 {1, 3, 6},
                 {1, 1, 5}
         };
     }
 
     @Test
-    public void whenA() {
+    public void whenXIs2AndYIs2() {
         int result = new Robot().optimalWay(board, 0, 0, 2, 2);
         assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenXIs1AndYIs1() {
+        int result = new Robot().optimalWay(board, 0, 0, 1, 1);
+        assertThat(result, is(2));
     }
 }
